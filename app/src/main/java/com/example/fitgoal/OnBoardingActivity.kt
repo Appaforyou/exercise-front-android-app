8170ec3fa9a3f0c3ed910f961844738bc6f1991e
Ruslan Tebyrtse <ruslan.tebyrtse@ripecode.dev>
2021-01-06 19:31:52 +0300

    finally onboarding

--- app/src/main/java/com/example/fitgoal/OnBoardingActivity.kt
+++ app/src/main/java/com/example/fitgoal/OnBoardingActivity.kt
@@ -1,17 +1,28 @@
 package com.example.fitgoal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.Window
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 
 class OnBoardingActivity : AppCompatActivity() {
 
     private  lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
+    private lateinit var indicatorsContainer: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
         setOnboardingItems()
+        setupIndicators()
+        setCurrentIndicator(0)
 
     }
 
@@ -34,5 +45,75 @@ class OnBoardingActivity : AppCompatActivity() {
         )
         val onboardingViewPager = findViewById<ViewPager2>(R.id.onboardingViewPager)
         onboardingViewPager.adapter = onboardingItemsAdapter
+        onboardingViewPager.registerOnPageChangeCallback(object :
+        ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+        (onboardingViewPager.getChildAt(0) as RecyclerView).overScrollMode =
+            RecyclerView.OVER_SCROLL_NEVER
+
+        findViewById<TextView>(R.id.textNext).setOnClickListener {
+            if (onboardingViewPager.currentItem + 1 < onboardingItemsAdapter.itemCount) {
+                onboardingViewPager.currentItem += 1
+            } else {
+                navigateToNextActivity()
+            }
+        }
+
+        findViewById<TextView>(R.id.textSkip).setOnClickListener {
+            navigateToNextActivity()
+        }
+    }
+
+    private fun navigateToNextActivity() {
+        startActivity(Intent(applicationContext, ActivityForFragments::class.java))
+        finish()
+    }
+
+    private fun setupIndicators() {
+        indicatorsContainer = findViewById(R.id.indicatorsContainer)
+        val indicators = arrayOfNulls<ImageView>(onboardingItemsAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8,0,8,0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let {
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+                it.layoutParams = layoutParams
+                indicatorsContainer.addView(it)
+            }
+        }
+
+    }
+
+    private fun setCurrentIndicator(position: Int) {
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainer.getChildAt(i) as ImageView
+            if (i == position) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+            }
+        }
     }
 }
