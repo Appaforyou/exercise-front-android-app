e0555daddba860c6cc8fd054d7cbb7e5050915b6
Ruslan Tebyrtse <ruslan.tebyrtse@ripecode.dev>
2021-01-06 13:11:31 +0300

    initial commit

---
+++ app/src/main/java/com/example/fitgoal/ActivityForFragments.kt
@@ -0,0 +1,18 @@
+package com.example.fitgoal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+
+
+class ActivityForFragments : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_for_fragments)
+
+
+        supportFragmentManager.commit {
+            replace(R.id.frameLayoutFragment, OnboardingFragment())
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fitgoal/OnboardingFragment.kt
@@ -0,0 +1,30 @@
+package com.example.fitgoal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class OnboardingFragment : Fragment() {
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_onboarding, container, false).apply {
+
+
+
+
+
+
+        }
+    }
+
+
+}
