99f823fa8cecdea42e4a03c1d66afd0352876ef0
Ruslan Tebyrtse <ruslan.tebyrtse@ripecode.dev>
2021-01-15 17:24:17 +0300

    finally splash screen

--- app/src/main/java/com/example/fitgoal/MainActivity.kt
+++ app/src/main/java/com/example/fitgoal/MainActivity.kt
@@ -3,10 +3,32 @@ package com.example.fitgoal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private fun openNextActivity() {
+        val intent = Intent(this, OnBoardingActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        object : CountDownTimer(2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("TAG", "TAG")
+            }
+
+            override fun onFinish() {
+                openNextActivity()
+            }
+
+        }.start()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,10 +36,6 @@ class MainActivity : AppCompatActivity() {
         window.statusBarColor = resources.getColor(R.color.color_top2)
         setTheme(R.style.Theme_FitGoal)
 
-        openNextActivity.setOnClickListener {
-            val intent = Intent(this, OnBoardingActivity::class.java)
-            startActivity(intent)
-        }
     }
 
 
