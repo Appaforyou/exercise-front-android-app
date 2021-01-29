ec20a9e7d246d3556c2c20e813b2630a951ef545
Ruslan Tebyrtse <ruslan.tebyrtse@ripecode.dev>
2021-01-29 10:10:14 +0300

    coroutine and gradle

---
+++ app/src/main/java/com/example/fitgoal/probe_coroutines.kt
@@ -0,0 +1,120 @@
+package com.example.fitgoal
+
+import kotlinx.coroutines.*
+
+
+fun main() {
+    GlobalScope.launch { // launch a new coroutine in background and continue
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,") // main thread continues here immediately
+    runBlocking {     // but this expression blocks the main thread
+        delay(2000L)  // ... while we delay for 2 seconds to keep JVM alive
+    }
+}
+
+
+//-----------------------------------------------------
+
+
+fun main1() = runBlocking<Unit> { // start main coroutine
+    GlobalScope.launch { // launch a new coroutine in background and continue
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,") // main coroutine continues here immediately
+    delay(2000L)      // delaying for 2 seconds to keep JVM alive
+}
+
+
+//-----------------------------------------------------
+
+
+fun main2() = runBlocking { // this: CoroutineScope
+    launch { // launch a new coroutine in the scope of runBlocking
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+}
+
+
+//-----------------------------------------------------
+
+
+fun main3() = runBlocking {
+//sampleStart
+    val job = GlobalScope.launch { // launch a new coroutine and keep a reference to its Job
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    job.join() // wait until child coroutine completes
+//sampleEnd
+}
+
+
+//-----------------------------------------------------
+
+
+fun main4() = runBlocking {
+    launch { doWorld() }
+    println("Hello,")
+}
+
+// this is your first suspending function
+suspend fun doWorld() {
+    delay(1000L)
+    println("World!")
+}
+
+
+//-----------------------------------------------------
+
+
+fun main5() = runBlocking {
+    repeat(100_000) { // launch a lot of coroutines
+        launch {
+            delay(5000L)
+            print(".")
+        }
+    }
+}
+
+
+//-----------------------------------------------------
+
+
+
+fun main6() = runBlocking {
+//sampleStart
+    GlobalScope.launch {
+        repeat(1000) { i ->
+            println("I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // just quit after delay
+//sampleEnd
+}
+
+
+//-----------------------------------------------------
+
+
+fun main7() = runBlocking {
+//sampleStart
+    val job = launch {
+        repeat(1000) { i ->
+            println("job: I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancel() // cancels the job
+    job.join() // waits for job's completion
+    println("main: Now I can quit.")
+//sampleEnd
+}
